e36a77b991104afaf653d151b5093141439d973d
Dmitry Les <dmitriy.s.les@gmail.com>
2014-11-13 18:03:41 +0200

    shops

--- mobile-config.js
+++ mobile-config.js
@@ -14,4 +14,16 @@ App.icons({
     'android_xhdpi': 'icons/android/xhdpi.png'
 });
 
+App.launchScreens({
+    'android_ldpi_portrait': 'launch_screens/android/ldpi.9.png',
+    'android_ldpi_landscape': 'launch_screens/android/ldpi.9.png',
+    'android_mdpi_portrait': 'launch_screens/android/mdpi.9.png',
+    'android_mdpi_landscape': 'launch_screens/android/mdpi.9.png',
+    'android_hdpi_portrait': 'launch_screens/android/hdpi.9.png',
+    'android_hdpi_landscape': 'launch_screens/android/hdpi.9.png',
+    'android_xhdpi_portrait': 'launch_screens/android/xhdpi.9.png',
+    'android_xhdpi_landscape': 'launch_screens/android/xhdpi.9.png'
+});
+
 App.setPreference('Orientation', 'portrait');
+App.setPreference('BackgroundColor', '0xFF3DA3A7');
