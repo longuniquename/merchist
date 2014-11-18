b7c995ef6e1990eecb0f1b01ceef2413d55dd9b9
Dmitry Les <dmitriy.s.les@gmail.com>
2014-11-18 12:29:26 +0200

    images

--- models/images.js
+++ models/images.js
@@ -1,10 +1,17 @@
+var imageStore = new FS.Store.S3("images", {
+    region:          "us-west-2",
+    accessKeyId:     "AKIAIXL525LMGYUSWYFQ",
+    secretAccessKey: "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",
+    bucket:          "merchist-staging"
+});
+
 Images = new FS.Collection("images", {
-    stores: [new FS.Store.FileSystem("images", {path: "../uploads"})],
+    stores: [imageStore],
     filter: {
         maxSize: 3145728,
-        allow: {
+        allow:   {
             contentTypes: ['image/*'],
-            extensions: ['png', 'PNG', 'jpg', 'JPG', 'jpeg', 'JPEG']
+            extensions:   ['png', 'PNG', 'jpg', 'JPG', 'jpeg', 'JPEG']
         }
     }
 });
