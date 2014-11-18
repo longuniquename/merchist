4de8f865e061aed8784942205a60515772024b1b
Dmitry Les <dmitriy.s.les@gmail.com>
2014-11-18 13:37:01 +0200

    products

--- client/management/products/edit/images/images.js
+++ client/management/products/edit/images/images.js
@@ -16,11 +16,24 @@
     };
 
     Template.managementProductsEditImages.helpers({
-
+        'image': function(){
+            Meteor.subscribe("image", this.imageId);
+            return Images.findOne(this.imageId);
+        }
     });
 
     Template.managementProductsEditImages.events({
+        'change #uploadLogoFile': function(e, template){
+            e.preventDefault();
 
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                Images.insert(e.target.result, function (err, fileObj) {
+                    Products.update(template.data._id, {$set: {imageId: fileObj._id}});
+                });
+            };
+            reader.readAsDataURL(e.currentTarget.files[0]);
+        }
     });
 
 })(Template);
