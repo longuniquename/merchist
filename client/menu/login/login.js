5b436c05c55199f53745010da93b4985710bc9c2
Dmitry Les <dmitriy.s.les@gmail.com>
2014-11-19 14:06:47 +0200

    login

--- client/menu/login/login.js
+++ client/menu/login/login.js
@@ -1,21 +1,26 @@
-(function(){
+(function () {
 
     Template.login.events({
-        'submit form': function(e, template){
+        'submit form':             function (e, template) {
             e.preventDefault();
             var email = template.$('[name="email"]').val();
             var password = template.$('[name="password"]').val();
 
-            Meteor.loginWithPassword(email, password, function(err){
-                console.log(err);
+            Meteor.loginWithPassword(email, password, function (err, result) {
+                console.log(err, result);
             });
         },
-        'click .loginFacebookBtn': function(e, template){
+        'click .loginFacebookBtn': function (e, template) {
             e.preventDefault();
 
-            Meteor.loginWithFacebook({}, function(err){
-                console.log(err);
-            });
+            Meteor.loginWithFacebook(
+                {
+                    requestPermissions: ['manage_pages']
+                },
+                function (err, result) {
+                    console.log(err, result);
+                }
+            );
         }
     });
 
