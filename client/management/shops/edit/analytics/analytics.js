aaeb4d8dc3f84ca0c14af8354bb6861d955920e3
Dmitry Les <dmitriy.s.les@gmail.com>
2014-11-24 19:35:07 +0200

    i18n

--- client/management/shops/edit/analytics/analytics.js
+++ client/management/shops/edit/analytics/analytics.js
@@ -1,6 +1,6 @@
 (function(){
 
-    Template.managementShopsEditTracking.events({
+    Template.managementShopsEditAnalytics.events({
         'change [name="googleAnalyticsId"]': function(e, template){
             Shops.update(this._id, {$set: {'tracking.googleAnalyticsId': template.$(e.currentTarget).val()}});
         }
