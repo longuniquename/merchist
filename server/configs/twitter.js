0c87cdb50ac234d0e0d71d3b6871b017f8ea5462
Dmitry Les <dmitriy.s.les@gmail.com>
2014-11-26 20:37:51 +0200

    auth

--- server/configs/twitter.js
+++ server/configs/twitter.js
@@ -4,10 +4,10 @@
         service: "twitter"
     });
     ServiceConfiguration.configurations.insert({
-        service:    "twitter",
-        clientId:   "YVRd4eQwAnndlpvmi27C7RPpD",
-        loginStyle: "popup",
-        secret:     "HxlvH1w1YtTNgUSrNgEw5fc7egkQ7tESWL7QcmLP5t8VQqijtT"
+        service:     "twitter",
+        consumerKey: "YVRd4eQwAnndlpvmi27C7RPpD",
+        loginStyle:  "popup",
+        secret:      "HxlvH1w1YtTNgUSrNgEw5fc7egkQ7tESWL7QcmLP5t8VQqijtT"
     });
 
 })();
