ee22a00df81b5adf3cf44fd47e986d3595992c90
Dmitry Les <dmitriy.s.les@gmail.com>
2014-11-26 19:33:45 +0200

    auth

--- client/dialogs/auth.js
+++ client/dialogs/auth.js
@@ -1,14 +1,14 @@
-(function(){
+(function () {
 
     Template.authDlg.events({
-        'submit form': function(e, template){
+        'submit form':        function (e, template) {
             e.preventDefault();
             var $dlg = $(template.firstNode);
 
             var email = template.$('[name="email"]').val(),
                 password = template.$('[name="password"]').val();
 
-            Meteor.loginWithPassword({email: email}, password, function(err){
+            Meteor.loginWithPassword({email: email}, password, function (err) {
                 if (!err) {
                     $dlg.modal('hide');
                 } else {
@@ -16,14 +16,23 @@
                 }
             });
         },
-        'click .facebookBtn': function(e, template){
+        'click .facebookBtn': function (e, template) {
             e.preventDefault();
-            Meteor.loginWithFacebook({
-                requestPermissions: ['email'],
-                loginStyle: 'popup'
-            }, function(){
-                console.log(arguments);
-            });
+            var $dlg = $(template.firstNode);
+
+            Meteor.loginWithFacebook(
+                {
+                    requestPermissions: ['email'],
+                    loginStyle:         'popup'
+                },
+                function (err) {
+                    if (!err) {
+                        $dlg.modal('hide');
+                    } else {
+                        console.log(err);
+                    }
+                }
+            );
         }
     });
 
