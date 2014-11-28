23fc988398a7d290ccd1e2335f36c55c3e5fe7c7
Dmitry Les <dmitriy.s.les@gmail.com>
2014-11-28 13:18:42 +0200

    Cordova plugins

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
     id:          'com.merchist.client',
-    version:     '0.0.6',
+    version:     '0.0.7',
     name:        'Merchist',
     description: 'Sell socially!',
     author:      'Mercher, Inc.',
