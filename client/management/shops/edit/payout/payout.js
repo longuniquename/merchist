c2ddf9d5c5a83cbf103780ad48dae801eeeeb560
Dmitry Les <dmitriy.s.les@gmail.com>
2014-12-01 18:59:53 +0200

    pp inapp

--- client/management/shops/edit/payout/payout.js
+++ client/management/shops/edit/payout/payout.js
@@ -10,7 +10,14 @@
             Meteor.call('PayPal:generateAccountRequest', this._id, Meteor.absoluteUrl('paypal/return'), Meteor.absoluteUrl('paypal/cancel'), function(err, url){
                 if (!err) {
                     $btn.button('redirecting');
-                    location.replace(url);
+                    if (Meteor.isCordova) {
+                        var ref = window.open(url, '_blank', 'location=yes');
+                        ref.addEventListener('exit', function(){
+                            $btn.button('reset').prop('disabled', false);
+                        });
+                    } else {
+                        location.replace(url);
+                    }
                 } else {
                     $btn.button('reset').prop('disabled', false);
                 }
