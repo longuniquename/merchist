2f256c6164413146e5b7de026099584b3b2da274
Dmitry Les <dmitriy.s.les@gmail.com>
2014-12-02 15:06:00 +0200

    og

--- client/marketplace/products/view/view.js
+++ client/marketplace/products/view/view.js
@@ -1,7 +1,39 @@
-(function(){
+(function () {
+
+    var namespaces = {
+            og:      'http://ogp.me/ns',
+            fb:      'http://ogp.me/ns/fb',
+            product: 'http://ogp.me/ns/product'
+        },
+        metaSelector = _.map(_.keys(namespaces), function (namespace) {
+            return 'meta[property^="' + namespace + ':"]';
+        }).join(', '),
+        $ogMeta;
+
+    Template.marketplaceProductsView.rendered = function () {
+        var $head = $('head'),
+            prefixes = [];
+
+        $ogMeta = $(metaSelector, $('body'));
+        $ogMeta.appendTo($head);
+
+        $ogMeta.each(function () {
+            var namespace = $(this).attr('property').split(':')[0];
+            if (_.has(namespaces, namespace)) {
+                prefixes.push(namespace + ': ' + namespaces[namespace] + '#');
+            }
+        });
+
+        $head.attr('prefix', _.uniq(prefixes).join(' '));
+    };
+
+    Template.marketplaceProductsView.destroyed = function () {
+        $($ogMeta).remove();
+        $('head').removeAttr('prefix');
+    };
 
     Template.marketplaceProductsView.helpers({
-        'image': function(){
+        'image': function () {
             Meteor.subscribe("image", this.imageId);
             return Images.findOne(this.imageId);
         }
