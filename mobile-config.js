ae395da55dee32384f13f066e075c7028d33ab9b
Dmitry Les <dmitriy.s.les@gmail.com>
2014-12-09 15:14:13 +0200

    ios launch screens

--- mobile-config.js
+++ mobile-config.js
@@ -13,14 +13,24 @@ App.icons({
     'android_mdpi':  'icons/android_mdpi.png',
     'android_hdpi':  'icons/android_hdpi.png',
     'android_xhdpi': 'icons/android_xhdpi.png',
-    'iphone':        'icons/iphone.png',
-    'iphone_2x':     'icons/iphone@2x.png',
-    'iphone_3x':     'icons/iphone@3x.png',
-    'ipad':          'icons/ipad.png',
-    'ipad_2x':       'icons/ipad@2x.png'
+    'iphone':        'icons/ios/iphone.png',
+    'iphone_2x':     'icons/ios/iphone_2x.png',
+    'iphone_3x':     'icons/ios/iphone_3x.png',
+    'ipad':          'icons/ios/ipad.png',
+    'ipad_2x':       'icons/ios/ipad_2x.png'
 });
 
 App.launchScreens({
+    'iphone':                  'launch_screens/ios/iphone.png',
+    'iphone_2x':               'launch_screens/ios/iphone_2x.png',
+    'iphone5':                 'launch_screens/ios/iphone5.png',
+    'iphone6':                 'launch_screens/ios/iphone6.png',
+    'iphone6p_portrait':       'launch_screens/ios/iphone6p_portrait.png',
+    'iphone6p_landscape':      'launch_screens/ios/iphone6p_landscape.png',
+    'ipad_portrait':           'launch_screens/ios/ipad_portrait.png',
+    'ipad_portrait_2x':        'launch_screens/ios/ipad_portrait_2x.png',
+    'ipad_landscape':          'launch_screens/ios/ipad_landscape.png',
+    'ipad_landscape_2x':       'launch_screens/ios/ipad_landscape_2x.png',
     'android_ldpi_portrait':   'launch_screens/android_ldpi_portrait.png',
     'android_ldpi_landscape':  'launch_screens/android_ldpi_landscape.png',
     'android_mdpi_portrait':   'launch_screens/android_mdpi_portrait.png',
