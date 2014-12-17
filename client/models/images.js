b8803837ca939cda6f68116e59fbaa9f6df8f178
Dmitry Les <dmitriy.s.les@gmail.com>
2014-12-17 16:20:25 +0200

    s3 secure

--- client/models/images.js
+++ client/models/images.js
@@ -1,9 +1,4 @@
-var imageStore = new FS.Store.S3("images", {
-    region:          "us-west-2",
-    accessKeyId:     "AKIAIXL525LMGYUSWYFQ",
-    secretAccessKey: "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",
-    bucket:          "merchist-staging"
-});
+var imageStore = new FS.Store.S3("images");
 
 Images = new FS.Collection("images", {
     stores: [imageStore],
--- server/models/images.js
+++ server/models/images.js
@@ -1,3 +1,21 @@
+var imageStore = new FS.Store.S3("images", {
+    region:          "us-west-2",
+    accessKeyId:     "AKIAIXL525LMGYUSWYFQ",
+    secretAccessKey: "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",
+    bucket:          "merchist-staging"
+});
+
+Images = new FS.Collection("images", {
+    stores: [imageStore],
+    filter: {
+        maxSize: 3145728,
+        allow:   {
+            contentTypes: ['image/*'],
+            extensions:   ['png', 'PNG', 'jpg', 'JPG', 'jpeg', 'JPEG']
+        }
+    }
+});
+
 Meteor.publish('images', function () {
     return Images.find();
 });
