6673c7d46cc819fdb429eb111d112503dd3806f4
Dmitry Les <dmitriy.s.les@gmail.com>
2014-12-23 18:48:00 +0200

    thumbs

--- client/models/images.js
+++ client/models/images.js
@@ -1,8 +1,12 @@
-var imageStore = new FS.Store.S3("images");
-var thumbsStore = new FS.Store.S3("thumbs");
+var oStore = new FS.Store.S3("o");
+var xsStore = new FS.Store.S3("xs");
+var sStore = new FS.Store.S3("s");
+var mStore = new FS.Store.S3("m");
+var lStore = new FS.Store.S3("l");
+var xlStore = new FS.Store.S3("xl");
 
 Images = new FS.Collection("images", {
-    stores: [imageStore, thumbsStore],
+    stores: [oStore, xsStore, sStore, mStore, lStore, xlStore],
     filter: {
         maxSize: 1024 * 1024 * 10,
         allow:   {
--- server/models/images.js
+++ server/models/images.js
@@ -1,26 +1,89 @@
-var imageStore = new FS.Store.S3("images", {
-    region:          "us-west-2",
-    accessKeyId:     "AKIAIXL525LMGYUSWYFQ",
-    secretAccessKey: "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",
-    bucket:          "merchist-staging"
-});
-
-var thumbsStore = new FS.Store.S3("thumbs", {
+var oStore = new FS.Store.S3("o", {
     region:          "us-west-2",
     accessKeyId:     "AKIAIXL525LMGYUSWYFQ",
     secretAccessKey: "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",
     bucket:          "merchist-staging",
     beforeWrite:     function (fileObj) {
-        fileObj.type('image/png', {store: "thumbs", save: false});
-        fileObj.name('thumb.png', {store: "thumbs", save: false});
+        fileObj.type('image/png', {store: "o", save: false});
+        fileObj.name('o.png', {store: "o", save: false});
+    },
+    transformWrite:  function (fileObj, readStream, writeStream) {
+        gm(readStream).stream('PNG').pipe(writeStream);
+    }
+});
+
+var xsStore = new FS.Store.S3("xs", {
+    region:          "us-west-2",
+    accessKeyId:     "AKIAIXL525LMGYUSWYFQ",
+    secretAccessKey: "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",
+    bucket:          "merchist-staging",
+    beforeWrite:     function (fileObj) {
+        fileObj.type('image/png', {store: "xs", save: false});
+        fileObj.name('xs.png', {store: "xs", save: false});
+    },
+    transformWrite:  function (fileObj, readStream, writeStream) {
+        gm(readStream).resize(100).stream('PNG').pipe(writeStream);
+    }
+});
+
+var sStore = new FS.Store.S3("s", {
+    region:          "us-west-2",
+    accessKeyId:     "AKIAIXL525LMGYUSWYFQ",
+    secretAccessKey: "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",
+    bucket:          "merchist-staging",
+    beforeWrite:     function (fileObj) {
+        fileObj.type('image/png', {store: "s", save: false});
+        fileObj.name('s.png', {store: "s", save: false});
     },
     transformWrite:  function (fileObj, readStream, writeStream) {
         gm(readStream).resize(200).stream('PNG').pipe(writeStream);
     }
 });
 
+var mStore = new FS.Store.S3("m", {
+    region:          "us-west-2",
+    accessKeyId:     "AKIAIXL525LMGYUSWYFQ",
+    secretAccessKey: "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",
+    bucket:          "merchist-staging",
+    beforeWrite:     function (fileObj) {
+        fileObj.type('image/png', {store: "m", save: false});
+        fileObj.name('m.png', {store: "m", save: false});
+    },
+    transformWrite:  function (fileObj, readStream, writeStream) {
+        gm(readStream).resize(400).stream('PNG').pipe(writeStream);
+    }
+});
+
+var lStore = new FS.Store.S3("l", {
+    region:          "us-west-2",
+    accessKeyId:     "AKIAIXL525LMGYUSWYFQ",
+    secretAccessKey: "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",
+    bucket:          "merchist-staging",
+    beforeWrite:     function (fileObj) {
+        fileObj.type('image/png', {store: "l", save: false});
+        fileObj.name('l.png', {store: "l", save: false});
+    },
+    transformWrite:  function (fileObj, readStream, writeStream) {
+        gm(readStream).resize(800).stream('PNG').pipe(writeStream);
+    }
+});
+
+var xlStore = new FS.Store.S3("xl", {
+    region:          "us-west-2",
+    accessKeyId:     "AKIAIXL525LMGYUSWYFQ",
+    secretAccessKey: "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",
+    bucket:          "merchist-staging",
+    beforeWrite:     function (fileObj) {
+        fileObj.type('image/png', {store: "xl", save: false});
+        fileObj.name('xl.png', {store: "xl", save: false});
+    },
+    transformWrite:  function (fileObj, readStream, writeStream) {
+        gm(readStream).resize(1600).stream('PNG').pipe(writeStream);
+    }
+});
+
 Images = new FS.Collection("images", {
-    stores: [imageStore, thumbsStore],
+    stores: [oStore, xsStore, sStore, mStore, lStore, xlStore],
     filter: {
         maxSize: 1024 * 1024 * 10,
         allow:   {
