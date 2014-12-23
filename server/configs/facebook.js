56e8db5be856bdbf16dbc7714b7eb28df7040d8a
Dmitry Les <dmitriy.s.les@gmail.com>
2014-12-23 19:59:41 +0200

    facebook config

--- server/configs/facebook.js
+++ server/configs/facebook.js
@@ -6,9 +6,9 @@
     ServiceConfiguration.configurations.insert({
         service:    "facebook",
         loginStyle: "popup",
-        appId:      process.env.FACEBOOK_APP_ID        || "301234113401207",
-        secret:     process.env.FACEBOOK_APP_SECRET    || "992c0edf51ed0ba7b6b0b057c76e255b",
-        namespace:  process.env.FACEBOOK_APP_NAMESPACE || "merchist_staging"
+        appId:      process.env.FACEBOOK_APP_ID        || "301234496734502",
+        secret:     process.env.FACEBOOK_APP_SECRET    || "303e8d4d5c2be57b604a95a9a2b1b827",
+        namespace:  process.env.FACEBOOK_APP_NAMESPACE || "merchist_local"
     });
 
 })();
