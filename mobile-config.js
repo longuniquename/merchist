06f8eaf0f8945f8155b04f5f2e5c579d0feee6a6
Dmitry Les <dmitriy.s.les@gmail.com>
2014-12-24 15:49:29 +0200

    mobile 0.1.10

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
     id:          'com.merchist.client',
-    version:     '0.1.9',
+    version:     '0.1.10',
     name:        'Merchist',
     description: 'Sell socially!',
     author:      'Mercher, Inc.',
