27e23bd45e3a988bf6f9cefb4e1d2ca85dc561f8
Dmitry Les <dmitriy.s.les@gmail.com>
2014-12-25 18:40:37 +0200

    profile fix

--- client/profile/details/basic_info/basic_info.js
+++ client/profile/details/basic_info/basic_info.js
@@ -1,29 +1,8 @@
-(function(){
+(function () {
 
     Template.profileDetailsBasicInfo.helpers({
-        schema: function(){
-            return new SimpleSchema({
-                'profile.firstName': {
-                    type:     String,
-                    label:    "First name",
-                    max:      32
-                },
-                'profile.lastName': {
-                    type:     String,
-                    label:    "Last name",
-                    max:      32
-                }
-            });
-        }
-    });
-
-    AutoForm.hooks({
-        basicInfoForm: {
-            onSubmit: function(insertDoc, updateDoc, currentDoc) {
-                this.event.preventDefault();
-                Meteor.users.update(Meteor.userId(), updateDoc);
-                this.done();
-            }
+        collection: function () {
+            return Meteor.users;
         }
     });
 
--- client/profile/details/seller_info/seller_info.js
+++ client/profile/details/seller_info/seller_info.js
@@ -1,31 +1,8 @@
-(function(){
+(function () {
 
     Template.profileDetailsSellerInfo.helpers({
-        schema: function(){
-            return new SimpleSchema({
-                'profile.email': {
-                    type:     String,
-                    label:    "Contact email",
-                    regEx:    SimpleSchema.RegEx.Email,
-                    optional: true
-                },
-                'profile.phone': {
-                    type:     String,
-                    label:    "Contact phone",
-                    max:      24,
-                    optional: true
-                }
-            });
-        }
-    });
-
-    AutoForm.hooks({
-        sellerInfoForm: {
-            onSubmit: function(insertDoc, updateDoc, currentDoc) {
-                this.event.preventDefault();
-                Meteor.users.update(Meteor.userId(), updateDoc);
-                this.done();
-            }
+        collection: function () {
+            return Meteor.users;
         }
     });
 
