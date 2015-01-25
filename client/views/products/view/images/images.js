6f885fe8e567f2d1362e4c2fd2afaf89b65a998d
Dmitry Les <dmitriy.s.les@gmail.com>
2015-01-25 18:25:26 +0200

    product images  view update

--- client/views/products/view/images/images.js
+++ client/views/products/view/images/images.js
@@ -1,44 +1,8 @@
 (function () {
 
-    var fadeToolbar = function () {
-        var $images = $('.images-block'),
-            top = $(window).scrollTop(),
-            height = $images.height(),
-            $mainToolbar = $('#mainToolbar');
-
-        if (top < height - 48) {
-            $mainToolbar.addClass('transparent');
-        } else {
-            $mainToolbar.removeClass('transparent');
-        }
-    };
-
-    var resizeImagesBlock = function () {
-        var $images = $('.images-block');
-
-        $images.css({
-            height: $images.width()
-        });
-    };
-
     Template.productViewImagesBlock.rendered = function () {
-        $(window).bind('resize', resizeImagesBlock);
-        $(window).bind('scroll', fadeToolbar);
-        resizeImagesBlock();
-        fadeToolbar();
+        this.$('#product-images-carousel .carousel-inner .item:first-child').addClass('active');
+        this.$('#product-images-carousel').carousel();
     };
-
-    Template.productViewImagesBlock.destroyed = function () {
-        $('#mainToolbar').removeClass('transparent');
-
-        $(window).unbind('resize', resizeImagesBlock);
-        $(window).unbind('scroll', fadeToolbar);
-    };
-
-    Template.productViewImagesBlock.helpers({
-        images: function () {
-            return Images.find({_id: {$in: this.imageIds}});
-        }
-    });
 
 })(Template);
