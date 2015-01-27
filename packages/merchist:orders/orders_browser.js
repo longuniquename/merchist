241ca37b4b27c8bc1241672a666b06a44395c419
Dmitry Les <dmitriy.s.les@gmail.com>
2015-01-27 13:02:25 +0200

    pp close flow

--- packages/merchist:orders/orders_browser.js
+++ packages/merchist:orders/orders_browser.js
@@ -1,11 +1,11 @@
 Order.prototype.pay = function (callback) {
     var self = this;
-    Meteor.call('Orders:getPayUrl', this._id, function(err, url) {
+    Meteor.call('Orders:getPayUrl', this._id, function (err, url) {
         if (!err) {
             var embeddedPPFlow = new PAYPAL.apps.DGFlow({expType: 'light'});
             embeddedPPFlow.startFlow(url);
             var handle = Orders.find({_id: self._id}).observeChanges({
-                changed: function(id, fields){
+                changed: function (id, fields) {
                     if (fields && fields.status && fields.status === "COMPLETED") {
                         handle.stop();
                         if (embeddedPPFlow.isOpen()) {
@@ -13,7 +13,20 @@ Order.prototype.pay = function (callback) {
                         }
                     }
                 }
-            })
+            });
+
+            $('iframe[name="PPDGFrame"]').on('load', function () {
+                try {
+                    var flowLocation = this.contentWindow.location.href;
+                } catch (e) {
+                }
+                if (flowLocation === Meteor.absoluteUrl('_orders/pay/close')) {
+                    handle.stop();
+                    if (embeddedPPFlow.isOpen()) {
+                        embeddedPPFlow.closeFlow();
+                    }
+                }
+            });
         }
     });
 };
