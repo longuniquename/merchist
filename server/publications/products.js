2301732190adbefb2d092f05994fbded7562aaf7
Dmitry Les <dmitriy.s.les@gmail.com>
2015-01-30 19:41:52 +0200

    watch product reservation

--- server/publications/products.js
+++ server/publications/products.js
@@ -13,34 +13,11 @@ Meteor.publish('products', function (options) {
 
     var ordersHandlers = {};
 
-    var initializing = true;
-
     var productsCursor = Products.find({isPublic: true}, options);
     var productsHandle = productsCursor.observeChanges({
         added:   function (id, product) {
-            //set initial isReserved value
-            if (initializing) {
-                product.isReserved = isReserved(id);
-            }
-
-            //watch orders to update isReserved value immediately
-            ordersHandlers[id] = Orders.find({
-                'items.productId': id,
-                'paypal.payKey':   {$exists: true}
-            }).observeChanges({
-                added:   function (orderId, order) {
-                    if (!initializing) {
-                        sub.changed('products', id, {isReserved: isReserved(id)});
-                    }
-                },
-                changed: function (orderId, fields) {
-                    sub.changed('products', id, {isReserved: isReserved(id)});
-                },
-                removed: function (orderId) {
-                    sub.changed('products', id, {isReserved: isReserved(id)});
-                }
-            });
-
+            product.isReserved = isReserved(id);
+            ordersHandlers[id] = watchReservations(id, sub);
             sub.added('products', id, product);
         },
         changed: function (id, fields) {
@@ -48,16 +25,18 @@ Meteor.publish('products', function (options) {
         },
         removed: function (id) {
             ordersHandlers[id] && ordersHandlers[id].stop();
+            delete ordersHandlers[id];
             sub.removed('products', id);
         }
     });
-
-    initializing = false;
 
     sub.ready();
 
     sub.onStop(function () {
         productsHandle.stop();
+        _.each(ordersHandlers, function (ordersHandler) {
+            ordersHandler.stop();
+        });
     });
 });
 
@@ -74,4 +53,51 @@ var isReserved = function (productId) {
         'items.productId': productId,
         'paypal.payKey':   {$exists: true}
     }).count();
+};
+
+var reservedDue = function (productId) {
+    var maxExpirationDate = new Date();
+    maxExpirationDate.setTime(0);
+
+    Orders.find({
+        'items.productId':             productId,
+        'paypal.payKey':               {$exists: true},
+        'paypal.payKeyExpirationDate': {$exists: true}
+    }).forEach(function (order) {
+        maxExpirationDate.setTime(Math.max(maxExpirationDate.getTime(), order.paypal.payKeyExpirationDate.getTime()));
+    });
+
+    if (maxExpirationDate.getTime() > (new Date()).getTime()) {
+        return maxExpirationDate;
+    } else {
+        return null;
+    }
+};
+
+var watchReservations = function (productId, sub) {
+    var changeTimeout;
+
+    var ordersHandler = Orders.find({
+        'items.productId':             productId,
+        'paypal.payKey':               {$exists: true},
+        'paypal.payKeyExpirationDate': {$exists: true}
+    }).observeChanges({
+        added:   function (orderId, order) {
+            var reservation = reservedDue(productId);
+            if (reservation) {
+                sub.changed('products', productId, {isReserved: true});
+            } else {
+                sub.changed('products', productId, {isReserved: false});
+            }
+        },
+        changed: function (orderId, fields) {
+            console.log(fields);
+            sub.changed('products', productId, {isReserved: isReserved(productId)});
+        },
+        removed: function (orderId) {
+            sub.changed('products', productId, {isReserved: isReserved(productId)});
+        }
+    });
+
+    return ordersHandler;
 };
