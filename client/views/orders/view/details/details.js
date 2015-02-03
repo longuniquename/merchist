3e56261761813f70221631f722f5cce0046f79cb
Dmitry Les <dmitriy.s.les@gmail.com>
2015-02-03 20:11:54 +0200

    order expiration

--- client/views/orders/view/details/details.js
+++ client/views/orders/view/details/details.js
@@ -1,5 +1,7 @@
 (function () {
 
+    var expiresIn = new ReactiveVar(null);
+
     Template.ordersViewDetails.helpers({
         'itemsCount': function () {
             var itemsCount = 0;
@@ -10,6 +12,24 @@
         },
         'payable':    function () {
             return this.status === 'NEW' || this.status === 'REQUESTED';
+        },
+        'expiresIn':    function () {
+            if (this.paypal && this.paypal.payKeyExpirationDate) {
+                var self = this;
+                Meteor.setTimeout(function(){
+                    var expirationMs = self.paypal.payKeyExpirationDate.getTime() - (new Date()).getTime(),
+                        expirationS = Math.floor(expirationMs / 1000),
+                        expirationM = Math.floor(expirationMs / 60000);
+                    if (expirationM > 0) {
+                        expiresIn.set(expirationM + ' minutes');
+                    } else if (expirationS > 0){
+                        expiresIn.set(expirationM + ' seconds');
+                    } else {
+                        expiresIn.set('Less then a second');
+                    }
+                }, 1000);
+            }
+            return expiresIn.get();
         }
     });
 
